2a27b0e92bf0afef899109b601c40030b74cbcf4
Yoni190 <yfisuhu@gmail.com>
2025-10-25 17:32:58 +0300

    Get base url from .env file

--- mobile/screens/HomeScreen.js
+++ mobile/screens/HomeScreen.js
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import * as SecureStore from "expo-secure-store"
+import axios from 'axios'
+import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const HomeScreen = () => {
+    const [user, setUser] = useState(null)
+
+    const navigation = useNavigation()
+
+    const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
+    
+
+    useEffect(() => {
+      const getUser = async () => {
+        const access_token = await SecureStore.getItemAsync("token")
+        if(!access_token) navigation.replace("Login")
+
+        try {
+            const response = await axios.get(`${API_URL}/user`, {
+                headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${access_token}`
+                }
+            })
+            setUser(response.data)
+        } catch (error) {
+            console.log(error)
+            navigation.navigate("Login")
+        }
+        
+      }
+
+      getUser()
+    }, [])
+    
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Greetings, {user && user.name}</Text>
+    </SafeAreaView>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
