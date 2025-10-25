55d641039e9bcaadcb74dde415431164416edb78
Yoni190 <yfisuhu@gmail.com>
2025-10-25 17:33:30 +0300

    Add loading style when Log in is clicked

--- mobile/screens/LoginScreen.js
+++ mobile/screens/LoginScreen.js
@@ -1,9 +1,51 @@
 import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import axios from 'axios'
+import * as SecureStore from "expo-secure-store"
 
 const LoginScreen = () => {
-const navigation = useNavigation()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errors, setErrors] = useState({})
+    const [loading, setLoading] = useState(false)
+    const navigation = useNavigation()
+
+    const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
+
+    const handleLogin = async () => {
+        setErrors({})
+        setLoading(true)
+        try {
+            const response = await axios.post(`${API_URL}/login`, {
+                email,
+                password,
+                device_name: `${Platform.OS} ${Platform.Version}`
+            }, {
+                headers: {
+                    Accept: "application/json"
+                }
+            })
+
+            const access_token = response.data
+            console.log(access_token)
+
+            await SecureStore.setItemAsync('token', access_token)
+            .then(() => navigation.replace("Home"))
+            .catch(err => console.error(err))
+
+            
+        } 
+        catch (error) {
+            if(error.response.status === 422){
+                setErrors(error.response.data)
+            }
+                
+        } finally {
+            setLoading(false)
+        } 
+    }
   return (
     <KeyboardAvoidingView
     behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -18,21 +60,36 @@ const navigation = useNavigation()
         keyboardType='email-address'
         style={styles.textInput}
         placeholderTextColor="#999"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        autoCapitalize='none'
       />
+      {errors && Object.keys(errors).length > 0 && errors.errors.email && (
+        <Text style={styles.errorText}>{errors.errors.email}</Text>
+      )}
+      
       <TextInput 
         placeholder='Password'
         secureTextEntry={true}
         style={styles.textInput}
         placeholderTextColor="#999"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
       />
+      {errors && Object.keys(errors).length > 0 && errors.errors.password && (
+        <Text style={styles.errorText}>{errors.errors.password}</Text>
+      )}
+
       <TouchableOpacity 
-      style={styles.btn}
+      style={[styles.btn, loading && {opacity: 0.6}]}
+      onPress={handleLogin}
+      disabled={loading}
       >
-        <Text style={styles.btnText}>Login</Text>
+        <Text style={styles.btnText}>{loading ? 'Loading...' : "Login"}</Text>
       </TouchableOpacity>
 
       <Text style={styles.footerText}>
-        Don't have an account? <Text style={styles.linkText} onPress={() => navigation.navigate("Register")}>Sign Up</Text>
+        Don't have an account? <Text style={styles.linkText} onPress={() => navigation.replace("Register")}>Sign Up</Text>
       </Text>
     </View>
     </KeyboardAvoidingView>
@@ -66,7 +123,7 @@ const styles = StyleSheet.create({
         paddingVertical: 12,
         paddingHorizontal: 15,
         fontSize: 16,
-        marginBottom: 15,
+        marginBottom: 10,
         backgroundColor: '#F5F5F5'
     },
     btn: {
@@ -100,5 +157,9 @@ const styles = StyleSheet.create({
     linkText: {
         color: '#111',
         fontWeight: 'bold'
+    },
+    errorText: {
+        color: "red",
+        marginBottom: 5
     }
 })
