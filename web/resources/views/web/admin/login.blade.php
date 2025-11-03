0e3cea677ab60261f33d2a1c45b57ce21f60f152
Yoni190 <yfisuhu@gmail.com>
2025-11-03 08:34:27 +0300

    Create login form

---
+++ web/resources/views/web/admin/login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+        body {
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <form action="/login" method="POST" class="border p-5 rounded shadow-lg d-flex flex-column">
+        <h2>Login</h2>
+        <input type="email" name="email" id="email" placeholder="Email Address" class="form-control mb-3">
+        <input type="password" name="password" id="password" placeholder="Password" class="form-control mb-3">
+        <button class="btn btn-primary">Log In</button>
+    </form>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
