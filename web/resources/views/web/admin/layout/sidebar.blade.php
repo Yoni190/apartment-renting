f9a00d99ababadc2f9aa06ae337b5932d60bcaf1
Yoni190 <yfisuhu@gmail.com>
2025-11-03 09:08:10 +0300

    Create sidebar component

---
+++ web/resources/views/web/admin/layout/sidebar.blade.php
@@ -0,0 +1,20 @@
+<div class="d-flex flex-column align-items-start px-3">
+    <h5 class="mb-4">Admin Menu</h5>
+    <ul class="nav nav-pills flex-column w-100">
+        <li class="nav-item mb-2">
+            <a href="#" class="nav-link text-dark">🏠 Dashboard</a>
+        </li>
+        <li class="nav-item mb-2">
+            <a href="#" class="nav-link text-dark">👥 Users</a>
+        </li>
+        <li class="nav-item mb-2">
+            <a href="#" class="nav-link text-dark">🏢 Apartments</a>
+        </li>
+        <li class="nav-item mb-2">
+            <a href="#" class="nav-link text-dark">⚙️ Settings</a>
+        </li>
+        <li class="nav-item mt-3">
+            <a href="#  " class="btn btn-outline-danger w-100">Logout</a>
+        </li>
+    </ul>
+</div>
