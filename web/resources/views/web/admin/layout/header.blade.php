3a4d617d4c731b4a80cc50c2b5d551cc3c02cc57
Yoni190 <yfisuhu@gmail.com>
2025-11-03 09:07:50 +0300

    Create header component

---
+++ web/resources/views/web/admin/layout/header.blade.php
@@ -0,0 +1,19 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-dark p-3">
+    <a href="#" class="navbar-brand text-white">Apartment Renting</a>
+    {{--
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a href="#" class="nav-link text-white">
+                Admin
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-white">
+                Setting
+            </a>    
+        </li>
+    </ul>
+    --}}    
+</nav>
