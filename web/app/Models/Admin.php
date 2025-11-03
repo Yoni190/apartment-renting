0c98405a0de34496aa9ec52b28086171548262f2
Yoni190 <yfisuhu@gmail.com>
2025-11-03 15:37:18 +0300

    Create admins table and model

---
+++ web/app/Models/Admin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\AdminRole;
+
+class Admin extends Authenticatable
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'admin_role_id'
+    ];
+
+
+    protected $hidden = [
+        'password'
+    ];
+
+
+    public function role() {
+        return $this->belongsTo(AdminRole::class, 'admin_role_id');
+    }
+}
