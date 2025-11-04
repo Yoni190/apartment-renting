2fd80624e30473c7a06ad2cd12dada477eebf0a6
Yoni190 <yfisuhu@gmail.com>
2025-11-04 18:37:30 +0300

    Add error alert catcher

--- web/resources/views/web/admin/layout/header.blade.php
+++ web/resources/views/web/admin/layout/header.blade.php
@@ -28,6 +28,15 @@
                         <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
                     </div>
                 </div>
+            @elseif(session('error'))
+                <div class="toast align-items-center text-bg-danger border-0 show" role="alert" aria-live="assertive" aria-atomic="true">
+                    <div class="d-flex">
+                        <div class="toast-body">
+                            {{ session('error') }}
+                        </div>
+                        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                    </div>
+                </div>
             @endif
     </div>
 
