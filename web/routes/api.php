69c1fa2f9cee4910451665939424b76fe8d7807a
Yoni190 <yfisuhu@gmail.com>
2025-11-25 15:58:45 +0300

    Create route to get apartments

--- web/routes/api.php
+++ web/routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
+use App\Models\Apartment;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Validation\Rules;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +22,14 @@ use Illuminate\Validation\Rules;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/apartment-list', function () {
+    $apartments = Apartment::all();
+
+    $apartments->load('images');
+
+    return $apartments;
 });
 
 
