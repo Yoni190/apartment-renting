d3fa3aa98df476e3ff6564cfe89174afaaca5ddf
Yoni190 <yfisuhu@gmail.com>
2026-01-16 12:10:14 +0300

    Fix national id controller path

--- external-services/routes/api.php
+++ external-services/routes/api.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NationalIdController;
+use App\Http\Controllers\Api\NationalIdController;
 
 Route::post('/verify-national-id', [NationalIdController::class, 'verify']);
