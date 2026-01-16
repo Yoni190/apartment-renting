bf508e55d312c3626889c45e7dbaa87c798e0db2
Yoni190 <yfisuhu@gmail.com>
2026-01-16 12:24:58 +0300

    Add test api

--- external-services/routes/api.php
+++ external-services/routes/api.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\NationalIdController;
 
 Route::post('/verify-national-id', [NationalIdController::class, 'verify']);
+
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'API is working!'
+    ]);
+});
