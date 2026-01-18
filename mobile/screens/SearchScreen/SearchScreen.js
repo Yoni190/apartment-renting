49e93f6afddf0166d4ff96ba01cafb4348764cd4
zuleyha <zuleyha.towfik-ug@aau.edu.et>
2026-01-18 10:00:00 +0300

    Add header to search screen

--- mobile/screens/SearchScreen/SearchScreen.js
+++ mobile/screens/SearchScreen/SearchScreen.js
@@ -1,12 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import styles from './SearchScreenStyle'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Header from '../../components/Header'
+
 
 const SearchScreen = () => {
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff', paddingTop: 50 }}>
+      {/* Add top padding so the absolute-positioned Header doesn't cover content */}
+      <Header 
+          title='Search'
+        />
+      <ScrollView showsVerticalScrollIndicator={false}> 
+        <Text>SearchScreen</Text>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
