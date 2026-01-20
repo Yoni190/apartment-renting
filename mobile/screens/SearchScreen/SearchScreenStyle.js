e43558d2185cd5054c3cd4535d6c5f7349dd0313
Yoni190 <yfisuhu@gmail.com>
2026-01-20 15:37:26 +0300

    Add placeholder when nothing is typed in search

--- mobile/screens/SearchScreen/SearchScreenStyle.js
+++ mobile/screens/SearchScreen/SearchScreenStyle.js
@@ -17,4 +17,24 @@ export default StyleSheet.create({
         fontSize: 16,
         color: '#000',
     },
+    placeholderContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 40,
+        },
+
+        placeholderTitle: {
+        marginTop: 16,
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#444',
+        },
+
+        placeholderText: {
+        marginTop: 8,
+        fontSize: 14,
+        color: '#888',
+        textAlign: 'center',
+        },
 })
