ba12e2b6a043236bb5aad77c70846ff530957ebf
Yoni190 <yfisuhu@gmail.com>
2026-01-24 08:31:14 +0300

    Create fillable columns for review table

--- web/app/Models/Review.php
+++ web/app/Models/Review.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'apartment_id',
+        'rating',
+        'comment'
+    ];
+
 }
