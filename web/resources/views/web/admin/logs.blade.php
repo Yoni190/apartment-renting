1a0b64b00e3e4a5367ea1f02723a3daa8f4f7deb
khalid <khalidwayu@gmail.com>
2026-01-25 10:00:00 +0300

    Create logs page

---
+++ web/resources/views/web/admin/logs.blade.php
@@ -0,0 +1,19 @@
+@extends('web.admin.layout.app')
+@section('title', 'Gojoye - Logs')
+
+
+@section('content')
+
+<div class="container mt-4">
+    <h1>Logs</h1>
+    <
+</div>
+
+@endsection
+
+@push('scripts')
+
+
+
+
+@endpush
