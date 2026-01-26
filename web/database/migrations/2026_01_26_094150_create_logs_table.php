f734aa924e9514b9deb6cdbfcf706a507a1973ee
Yoni190 <yfisuhu@gmail.com>
2026-01-26 12:46:48 +0300

    Create migration for logs table

---
+++ web/database/migrations/2026_01_26_094150_create_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('admin_id')
+                  ->constrained('admins')
+                  ->cascadeOnDelete();
+
+            $table->string('entity_type');
+            $table->unsignedBigInteger('entity_id')->nullable();
+            $table->string('action');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+    }
+};
