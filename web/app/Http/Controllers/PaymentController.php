4e0664e97e444a670300216f773e09a8bef99311
yoni <yfisuhu@gmail.com>
2026-04-08 11:01:32 +0300

    Create index function

--- web/app/Http/Controllers/PaymentController.php
+++ web/app/Http/Controllers/PaymentController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = auth()->user();
+
+        // Optional: redirect subscribed users
+        if ($user->subscribed) {
+            return redirect()->route('user.client.dashboard')
+                             ->with('info', 'You are already subscribed.');
+        }
+
+        return view('web.client.subscription'); // Make sure this Blade exists
+    }
 }
