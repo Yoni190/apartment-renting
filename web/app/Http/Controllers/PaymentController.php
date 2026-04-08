ac297c04d61d1797d7cd5d891e90f6a717ae4223
yoni <yfisuhu@gmail.com>
2026-04-08 11:25:08 +0300

    Add initialize payment function

--- web/app/Http/Controllers/PaymentController.php
+++ web/app/Http/Controllers/PaymentController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Chapa\Chapa\Facades\Chapa as Chapa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class PaymentController extends Controller
 {
+    protected $reference;
+
+    public function __construct()
+    {
+        $this->reference = Chapa::generateReference();
+    }
+
     public function index()
     {
         $user = auth()->user();
@@ -16,5 +26,37 @@ class PaymentController extends Controller
         }
 
         return view('web.owner.subscription');
+    }
+
+    public function initialize(Request $request)
+    {
+        $user = Auth::user();
+
+        $amount = $request->input('amount'); // 2000 or 5000
+        $planType = $request->input('plan_type'); // basic or premium
+
+        $reference = $this->reference;
+
+        $data = [
+            'amount' => $amount,
+            'email' => $user->email,
+            'tx_ref' => $reference,
+            'currency' => "ETB",
+            'callback_url' => route('callback', [$reference]),
+            'first_name' => $user->name,
+            'last_name' => '',
+            "customization" => [
+                "title" => ucfirst($planType) . ' Plan Subscription',
+                "description" => "Subscription for {$planType} plan"
+            ]
+        ];
+
+        $payment = Chapa::initializePayment($data);
+
+        if ($payment['status'] !== 'success') {
+            return redirect()->back()->with('error', 'Something went wrong while initiating payment.');
+        }
+
+        return redirect($payment['data']['checkout_url']);
     }
 }
