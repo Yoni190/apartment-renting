72299b36879d098df44b17dbdf9fcd7c1188ec3f
khalid <khalidwayu@gmail.com>
2026-05-05 18:33:45 +0300

    Redirect to admin login if trying to access telescope

--- web/app/Http/Middleware/Authenticate.php
+++ web/app/Http/Middleware/Authenticate.php
@@ -12,6 +12,10 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
+        if ($request->is('gojoye-telescope') || $request->is('gojoye-telescope/*')) {
+            return route('admin.login');
+        }
+        
         return $request->expectsJson() ? null : route('login');
     }
 }
