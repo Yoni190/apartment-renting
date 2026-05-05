345553f0fd3fb870d22ee1a2757636627de8a36f
yitbarek <Yitdan17@gmail.com>
2026-05-05 17:19:02 +0300

    Set the logo as the favicon

--- web/resources/views/web/client/layout/app.blade.php
+++ web/resources/views/web/client/layout/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-
+    <link rel="icon" type="image/png" href="{{ asset('images/logo2.png') }}">
 
     <!-- Custom Styles -->
     <!-- <link rel="stylesheet" href="{{ asset('css/main.css') }}"> -->
